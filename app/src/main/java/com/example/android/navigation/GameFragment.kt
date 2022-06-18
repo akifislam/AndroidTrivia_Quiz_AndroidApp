392d2339a0e422511eb409471ae7a50c626347ce
Akif Islam <iamakifislam@gmail.com>
2022-06-18 17:30:39 +0600

    question changed

--- app/src/main/java/com/example/android/navigation/GameFragment.kt
+++ app/src/main/java/com/example/android/navigation/GameFragment.kt
@@ -28,33 +28,63 @@ import com.example.android.navigation.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
     data class Question(
-            val text: String,
-            val answers: List<String>)
+        val text: String,
+        val answers: List<String>
+    )
 
     // The first answer is the correct one.  We randomize the answers before showing the text.
     // All questions must have four answers.  We'd want these to contain references to string
     // resources so we could internationalize. (or better yet, not define the questions in code...)
     private val questions: MutableList<Question> = mutableListOf(
-            Question(text = "What is Android Jetpack?",
-                    answers = listOf("all of these", "tools", "documentation", "libraries")),
-            Question(text = "Base class for Layout?",
-                    answers = listOf("ViewGroup", "ViewSet", "ViewCollection", "ViewRoot")),
-            Question(text = "Layout for complex Screens?",
-                    answers = listOf("ConstraintLayout", "GridLayout", "LinearLayout", "FrameLayout")),
-            Question(text = "Pushing structured data into a Layout?",
-                    answers = listOf("Data Binding", "Data Pushing", "Set Text", "OnClick")),
-            Question(text = "Inflate layout in fragments?",
-                    answers = listOf("onCreateView", "onViewCreated", "onCreateLayout", "onInflateLayout")),
-            Question(text = "Build system for Android?",
-                    answers = listOf("Gradle", "Graddle", "Grodle", "Groyle")),
-            Question(text = "Android vector format?",
-                    answers = listOf("VectorDrawable", "AndroidVectorDrawable", "DrawableVector", "AndroidVector")),
-            Question(text = "Android Navigation Component?",
-                    answers = listOf("NavController", "NavCentral", "NavMaster", "NavSwitcher")),
-            Question(text = "Registers app with launcher?",
-                    answers = listOf("intent-filter", "app-registry", "launcher-registry", "app-launcher")),
-            Question(text = "Mark a layout for Data Binding?",
-                    answers = listOf("<layout>", "<binding>", "<data-binding>", "<dbinding>"))
+        Question(
+            text = "What is the captial of Bangladesh?",
+            answers = listOf("Dhaka", "Khulna", "Natore", "Rajshahi")
+        ),
+        Question(
+            text = "What is the captial of India?",
+            answers = listOf("New Delhi", "Mumbai", "Kolkata", "Chennai")
+        ),
+        Question(
+            text = "What is the captial of Pakistan?",
+            answers = listOf("Islamabad", "Karachi", "Lahore", "Peshawar")
+        ),
+        Question(
+            text = "Which department is the best in RU?",
+            answers = listOf("CSE", "EEE", "MSE", "ICE")
+        ),
+        Question(
+            text = "What is the student ID of Akif?",
+            answers = listOf("35", "10", "53", "17")
+        ),
+        Question(
+            text = "Which comes first? Egg or Hen?",
+            answers = listOf("Amar kono Lav loss nai, Life tai Loss", "Egg", "Hen", "Adrita")
+        ),
+        Question(
+            text = "Who takes our Soft Dev III course?",
+            answers = listOf("Faruqe Sir", "Somlal Sir", "Asif Zaman Sir", "BKP Sir")
+        ),
+        Question(
+            text = "x = (n-1) | In which side it is shifted according to DSP?",
+            answers = listOf(
+                "Right Shifted",
+                "Left Shifted",
+                "Shift Bolte Kichu Nai",
+                "Not Shifted"
+            )
+        ),
+        Question(
+            text = "Who created Kotlin?",
+            answers = listOf("JetBrains", "Google", "Facebook", "Microsoft")
+        ),
+        Question(
+            text = "Who is the future billionaire of the world?",
+            answers = listOf("Akif Islam", "Warren Buffett", "Mark Zuckerberg", "Bill Gates")
+        ),
+        Question(
+            text = "Which is the best tissue-thief in CSE RU 27?",
+            answers = listOf("Farjana", "Srity", "Adrita", "Rupa")
+        ),
     )
 
     lateinit var currentQuestion: Question
@@ -62,12 +92,15 @@ class GameFragment : Fragment() {
     private var questionIndex = 0
     private val numQuestions = Math.min((questions.size + 1) / 2, 3)
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
 
         // Inflate the layout for this fragment
         val binding = DataBindingUtil.inflate<FragmentGameBinding>(
-                inflater, R.layout.fragment_game, container, false)
+            inflater, R.layout.fragment_game, container, false
+        )
 
         // Shuffles the questions and sets the question index to the first question.
         randomizeQuestions()
@@ -98,7 +131,8 @@ class GameFragment : Fragment() {
                         binding.invalidateAll()
                     } else {
                         // We've won!  Navigate to the gameWonFragment.
-                        view.findNavController().navigate(R.id.action_gameFragment_to_gameWonFragment)
+                        view.findNavController()
+                            .navigate(R.id.action_gameFragment_to_gameWonFragment)
                     }
                 } else {
                     // Game over! A wrong answer sends us to the gameOverFragment.
@@ -124,6 +158,7 @@ class GameFragment : Fragment() {
         answers = currentQuestion.answers.toMutableList()
         // and shuffle them
         answers.shuffle()
-        (activity as AppCompatActivity).supportActionBar?.title = getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
+        (activity as AppCompatActivity).supportActionBar?.title =
+            getString(R.string.title_android_trivia_question, questionIndex + 1, numQuestions)
     }
 }
